dbfdb41e54cc51ee8de32dc819f25b08fb802522
franciscotpezzi <fthunderpezzi@gmail.com>
2021-05-28 18:11:31 -0300

    atualização

--- funcao.js
+++ funcao.js
@@ -14,3 +14,35 @@ function quadrado() {
 function limpa() {
     resultado.innerHTML = ` &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914`
 }
+
+function tabuada() {
+    var num = window.document.getElementById('num')
+    var tab = window.document.getElementById('respT')
+    var n = Number(num.value)
+    var i = 1
+    tab.innerHTML =''
+    while (i <=10){
+        tab.innerHTML += `<strong>${n} x ${i} = ${n*i}</strong></br>`
+        i++
+    }
+}
+
+function limpaT() {
+    document.getElementById('num').value = ''
+    respT.innerHTML=`&#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914`
+}
+
+function soma100() {
+    let res = window.document.getElementById('resS100')
+    let soma = 0
+    let cont = 1
+    while (cont <= 100){
+        soma += cont
+        cont ++
+    }
+    res.innerHTML = `<strong>${soma}</strong>`
+}
+
+function limpa100() {
+    resS100.innerHTML = ` &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914` 
+}
