cd86f38160b08b1a0589d556ecb1a313ee0a08f1
franciscotpezzi <fthunderpezzi@gmail.com>
2021-05-31 12:41:37 -0300

    exercício 04

--- funcao.js
+++ funcao.js
@@ -46,3 +46,21 @@ function soma100() {
 function limpa100() {
     resS100.innerHTML = ` &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914 &#x1f914` 
 }
+
+function som500p(){
+    let res500p = window.document.getElementById('res500p')
+    let i = 1
+    let soma = 0
+    while (i <= 500){
+        if(i % 2 == 0){
+            soma += i
+        }
+        i++
+    }
+    res500p.innerHTML =`O resultado é <strong>${soma}</strong>`
+
+}
+
+function limpa500p(){
+    res500p.innerHTML=`&#129409  &#129409  &#129409  &#129409`
+}
