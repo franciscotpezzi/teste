da4b052dce9a75631739229419b66b62275a7bc2
franciscotpezzi <fthunderpezzi@gmail.com>
2021-06-01 22:10:49 -0300

    atualizando o dia

--- JS/leone.js
+++ JS/leone.js
@@ -19,7 +19,7 @@ function pot3_15(){
         if(cont >= 2){
             n *= 3 
         }
-        resp3_15.innerHTML+=` ${n} &#x1f4a1`
+        resp3_15.innerHTML+=`3<sup>${cont}</sup> = ${n} &#x1f4a1`
         cont += 1
     }
 
@@ -27,4 +27,59 @@ function pot3_15(){
 function limpot3_15(){
     resp3_15.innerHTML=`&#x1f914  &#x1f914  &#x1f914`
 
+}
+
+function calcPot(){
+    let exit = window.document.getElementById('resPot')
+    let bn = window.document.getElementById('base')
+    let en = window.document.getElementById('exp')
+    let b = Number(bn.value)
+    let e = Number(en.value)
+    let res = 1
+    let i = 0
+    
+        if(e == 0){
+            res = 1
+        }
+        if(e == 1){
+            res = b
+        }
+        if(e >=2){
+            for(let i = 1; i <= e; i++){
+                res *= b
+            }
+        }
+        exit.innerHTML = `${res}`
+        i ++
+    
+    e = 0
+    b = 0
+    }
+    function limPot(){
+    resPot = window.document.getElementById('resPot').innerHTML='&#x1f436  &#x1f436  &#x1f436'
+    document.getElementById('base').value =`0`;   
+    document.getElementById('exp').value =`0`;   
+    
+    }
+
+    
+function fibonacci(){
+    let tela = window.document.getElementById('tela')
+    let ant = 1
+    let atu = 0
+    let pro = 0
+    let i = 1
+    tela.innerHTML=' '
+
+    while( i <= 15){
+        
+        tela.innerHTML +=` ${pro}`
+        pro = (atu + ant)
+        atu = ant
+        ant = pro
+        i++
+    }
+}
+function limpar(){
+    tela.innerHTML=`&#x1f58c  &#x1f58c   &#x1f58c   &#x1f58c   &#x1f58c   &#x1f58c   &#x1f58c`
 }
